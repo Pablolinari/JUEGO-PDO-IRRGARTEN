dbaa52658542ef0cc42ce89e235eaa5a2d7fa1ca
Pablolinari <paablolinaari@gmail.com>
2024-02-27 15:19:11 +0100

    .

--- src/juego/JUEGOPDOJ.java
+++ src/juego/JUEGOPDOJ.java
@@ -13,7 +13,7 @@ public class JUEGOPDOJ {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
         
     }
     
