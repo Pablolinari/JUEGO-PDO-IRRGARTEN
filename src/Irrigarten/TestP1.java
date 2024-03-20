a042b2e5a38977278cfae550bcdc462435774356
Pablolinari <paablolinaari@gmail.com>
2024-03-20 21:32:11 +0100

    .

--- src/Irrigarten/TestP1.java
+++ src/Irrigarten/TestP1.java
@@ -14,7 +14,6 @@ public class TestP1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Dice dado = new Dice();
         Weapon arma = new Weapon(5.23f, 5);
         Shield escudo = new Shield(3.21f, 3);
         GameState juego = new GameState("laberinto", "pablo,pedro,pepe,jose", "monstruo1,monstruo2", 3, false,
@@ -28,14 +27,14 @@ public class TestP1 {
         System.out.printf("PRUEBA TOSTRING : %s\n", escudo.toString());
         System.out.printf("PRUEBA DISCARD : %b\n", escudo.discard());
 
-        System.out.printf("PRUEBA weaponsreward %d\n", dado.weaponsReward());
-        System.out.printf("PRUEBA shieldsreward %d\n", dado.shieldsReward());
-        System.out.printf("PRUEBA healthreward %d\n", dado.healthReward());
-        System.out.printf("PRUEBA weaponpower %f\n", dado.weaponPower());
-        System.out.printf("PRUEBA shieldpower %f\n", dado.shieldPower());
-        System.out.printf("PRUEBA usesleft %d\n", dado.usesLeft());
-        System.out.printf("PRUEBA intensity %f\n", dado.intensity(3.45f));
-        System.out.printf("PRUEBA discardelement %b\n", dado.discardElement(6));
+        System.out.printf("PRUEBA weaponsreward %d\n", Dice.weaponsReward());
+        System.out.printf("PRUEBA shieldsreward %d\n", Dice.shieldsReward());
+        System.out.printf("PRUEBA healthreward %d\n", Dice.healthReward());
+        System.out.printf("PRUEBA weaponpower %f\n", Dice.weaponPower());
+        System.out.printf("PRUEBA shieldpower %f\n", Dice.shieldPower());
+        System.out.printf("PRUEBA usesleft %d\n", Dice.usesLeft());
+        System.out.printf("PRUEBA intensity %f\n", Dice.intensity(3.45f));
+        System.out.printf("PRUEBA discardelement %b\n", Dice.discardElement(6));
 
         System.out.printf("PRUEBA getlabrint %s\n", juego.getLabyrinthv());
         System.out.printf("PRUEBA getplayers %s\n", juego.getPlayers());
