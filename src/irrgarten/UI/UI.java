e9824e544a5531bc0eecad6505322b248c646386
Pablolinari <paablolinaari@gmail.com>
2024-05-21 12:20:16 +0200

    .

--- src/irrgarten/UI/UI.java
+++ src/irrgarten/UI/UI.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package irrgarten.UI;
 import irrgarten.Directions;
 import irrgarten.GameState;
 
-
-/**
- *
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public interface UI {
     
     public Directions nextMove();
